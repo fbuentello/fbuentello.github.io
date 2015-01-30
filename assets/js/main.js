b2b486521b388398af96c0b9c878f1ecf66e09ba
Fabian Buentello <fbuentello@mioty.com>
2015-01-29 21:40:03 -0600

    Adding my existing site.

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+	if ($(".navbar").offset().top > 50) {
+		$(".navbar-fixed-top").addClass("top-nav-collapse");
+	} else {
+		$(".navbar-fixed-top").removeClass("top-nav-collapse");
+	}
+});
+
+
+
+
+$(document).ready(function() {
+
+	var scrollTo = function (pageSection) {
+		$('html,body').animate({
+			scrollTop: $(""+pageSection).offset().top},
+			1000);
+	}
+	$("#scrollDown").click(function() {
+		scrollTo('#profile');
+	});
+	$("#navProfile").click(function() {
+		scrollTo("#profile");
+	});
+	$("#navSkills").click(function() {
+		scrollTo("#skills");
+	});
+	$("#navResume").click(function() {
+		scrollTo("#resume");
+	});
+
+});
