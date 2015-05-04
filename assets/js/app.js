665ae3c2c9951a9f8b319a0d1fbc96c761f755fa
Fabian Buentello <fbuentello@mioty.com>
2015-05-04 14:09:51 -0500

    abstracting all personal info

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+
+app =angular.module('myApp', []);
+
+app.factory('DataFactory', ['$http','$q', function ($http, $q) {
+
+	return {
+		getData: $http.get('personalData.json')
+	};
+}]);
+
+app.controller('MainController', ['$scope','DataFactory', function($scope,DataFactory){
+
+DataFactory.getData.success(function(response){
+	$scope.personalInfo = response.personalInfo;
+	$scope.skills = response.skills;
+});
+
+
+}]);
