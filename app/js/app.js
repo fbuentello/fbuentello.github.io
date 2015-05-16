9dec5f9578f80070cf277496c193dd331b65333a
Fabian Buentello <fbuentello@mioty.com>
2015-05-15 21:49:06 -0500

    update from ait

--- app/js/app.js
+++ app/js/app.js
@@ -3,29 +3,36 @@
 *
 * Fabians Personal Website
 */
-app = angular.module('myApp', ['ngRoute']);
+app = angular.module('myApp', ['ngRoute','ngSanitize']);
 app.controller('mainController', ['$scope', function($scope){
 
 	$scope.languages = [{
-		name:'Javascript',
+		name:'Javascript(Frontend/Backend)',
 		exp: '90%',
 		color: '#c8102e',
-		text: "Aliquam in pharetra leo. In congue, massa sed elementum dictum, justo quam efficitur risus, in posuere mi orci ultrices diam."
-	},
-	{
+		text: "<h2 class='heading multiColor tileHeader'> Back end </h2><h5 class='heading multiColor tileHeader'>Go to Frameworks</h5><ul><li>NodeJS</li><li>SailsJS</li></ul><p>With excitment surrounding io.js, I'm excited for Node's future. ps: I can build one mean <a class='multiColor' href='http://www.sitepoint.com/introduction-mean-stack/'>MEAN Stack</a> app ;). </p><h2 class='heading multiColor tileHeader'> Front end </h2><h5 class='heading multiColor tileHeader'>Go to Frameworks</h5><ul><li>AngularJS</li><li>EmberJS</li><li>The emphamis <a class='multiColor' href='http://tholman.com/elevator.js/'>ElevatorJS</a> :P</li></ul><p>With new frameworks coming out every 7th minute of every hour. It's not so much about what Javascript frameworks you know, but how fast can you adopt a new one.</p>"
+	}, {
 		name:'Objective-C/Swift',
 		exp: '95%',
 		color: '#9c27b0',
-		text: "Aliquam in pharetra leo. In congue, massa sed elementum dictum, justo quam efficitur risus, in posuere mi orci ultrices diam."
-	},
-	{
+		text: "<h2 class='heading multiColor tileHeader'>[[Speak Brackets] toMe];</h2><h5 class='heading multiColor tileHeader'>Go to Frameworks</h5><ul><li><a class='multiColor' href='https://github.com/Mantle/Mantle'>Mantle</a></li><li><a class='multiColor' href='https://github.com/ReactiveCocoa/ReactiveCocoa'>ReactiveCocoa</a></li></ul><p> first and foremost, Objective-C is my first passion. I learned it before I learned any other programming language. I had no idea it was one of the most <a class='multiColor' href='https://youtu.be/73uu9YTq_1M'>difficult languages to learn</a>. I use this as motivation when I come across obstacles. Now with swift making its way in, programming for Apple devices has never been easier. </p>"
+	}, {
 		name:'Android',
 		exp: '50%',
 		color: '#4caf50',
-		text: "Aliquam in pharetra leo. In congue, massa sed elementum dictum, justo quam efficitur risus, in posuere mi orci ultrices diam."
-	},
-	{
+		text: "<h2 class='heading multiColor tileHeader'>[[Speak Brackets] toMe];</h2><h5>Go to Frameworks</h5><ul><li><a class='multiColor' href='https://github.com/Mantle/Mantle'>Mantle</a></li><li><a class='multiColor' href='https://github.com/ReactiveCocoa/ReactiveCocoa'>ReactiveCocoa</a></li></ul><p> first and foremost, Objective-C is my first passion. I learned it before I learned any other programming language. I had no idea it was one of the most <a href='https://youtu.be/73uu9YTq_1M'>difficult languages to learn</a>. I use this as motivation when I come across obstacles. Now with swift making its way in, programming for Apple devices has never been easier. </p>"
+	}, {
 		name:'HTML/CSS',
+		exp: '85%',
+		color: '#2196f3',
+		text: "Aliquam in pharetra leo. In congue, massa sed elementum dictum, justo quam efficitur risus, in posuere mi orci ultrices diam."
+	}, {
+		name:'Ruby on Rails',
+		exp: '67%',
+		color: '#2196f3',
+		text: "Aliquam in pharetra leo. In congue, massa sed elementum dictum, justo quam efficitur risus, in posuere mi orci ultrices diam."
+	}, {
+		name:'PHP',
 		exp: '85%',
 		color: '#2196f3',
 		text: "Aliquam in pharetra leo. In congue, massa sed elementum dictum, justo quam efficitur risus, in posuere mi orci ultrices diam."
@@ -66,31 +73,21 @@ app.controller('mainController', ['$scope', function($scope){
 	var lightColors = [
 	'#F44336',
 	'#9C27B0',
-	'#673AB7',
 	'#3F51B5',
 	'#2196F3',
-	'#03A9F4',
 	'#009688',
 	'#00BCD4',
-	'#4CAF50',
-	'#FF5722',
-	'#9E9E9E',
-	'#607D8B'
+	'#FF5722'
 	];
 
 	var darkColors = [
 	'#B43128',
 	'#631970',
-	'#432677',
 	'#293475',
 	'#186FB3',
-	'#027DB4',
 	'#00564E',
 	'#008394',
-	'#306F33',
-	'#BF4119',
-	'#5E5E5E',
-	'#34444B'
+	'#BF4119'
 	];
 	var randNum = Math.floor(Math.random() * lightColors.length);
 	$scope.pageColor = lightColors[randNum];
